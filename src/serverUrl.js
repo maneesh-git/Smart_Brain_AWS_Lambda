9643b08755dfb09ad1fc4cad1d0ddf10ff127cf8
Maneesh <maneeshtiwari.2020@gmail.com>
2021-07-23 20:10:46 +0530

    Final commit

---
+++ src/serverUrl.js
@@ -0,0 +1,8 @@
+// This app works with a amazon lambda function which will reflect an emoji in the Rank Component.
+// Do add the amazon lambda url in the Rank component.
+
+// Add the backend server URL in the following line : 
+
+const backendUrl = 'http://localhost:3000';
+
+export default backendUrl;
